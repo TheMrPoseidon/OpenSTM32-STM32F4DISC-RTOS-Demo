ba080074563f2faca609c9d5517729e46dbc4879
Christian Himmler <himmlerch58130@th-nuernberg.de>
2018-05-28 17:38:52 +0200

    Add Accelerometer

--- src/main.c
+++ src/main.c
@@ -11,21 +11,25 @@
 
 #include "hardware.h"
 
+int16_t x, y, z;
+
 static void prvSetupHardware( void );
 void prvATask(void *pvParameter);
 void prvBTask(void *pvParameter);
 void prvCTask(void *pvParameter);
 void prvDTask(void *pvParameter);
+void prvAccelerometerTask( void *pvParameters );
 
 int main(void)
 {
 	/* Setup the microcontroller hardware for the demo. */
 	prvSetupHardware();
-
+	BSP_ACCELERO_Init();
 
 	/*
 	 * Create the first LED Task
 	 */
+	xTaskCreate(prvAccelerometerTask, "AcceleroTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	xTaskCreate(prvATask, "ATask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	xTaskCreate(prvBTask, "BTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	xTaskCreate(prvCTask, "CTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
@@ -40,13 +44,15 @@ int main(void)
 
 void prvATask( void *pvParameters )
 {
-	BSP_LED_Init(LED3);
+	BSP_LED_Init(LED5);
 	for( ;; )
 	{
-		BSP_LED_Toggle(LED3);
-		vTaskDelay(21);
+		if(x > 100)
+			BSP_LED_On(LED5);
+		else
+			BSP_LED_Off(LED5);
+		vTaskDelay(50);
 	}
-
 }
 
 void prvBTask( void *pvParameters )
@@ -54,19 +60,25 @@ void prvBTask( void *pvParameters )
 	BSP_LED_Init(LED4);
 	for( ;; )
 	{
-		BSP_LED_Toggle(LED4);
-		vTaskDelay(42);
+		if(x <  -100)
+			BSP_LED_On(LED4);
+		else
+			BSP_LED_Off(LED4);
+		vTaskDelay(50);
 	}
 
 }
 
 void prvCTask( void *pvParameters )
 {
-	BSP_LED_Init(LED5);
+	BSP_LED_Init(LED3);
 	for( ;; )
 	{
-		BSP_LED_Toggle(LED5);
-		vTaskDelay(84);
+		if(y > 100)
+			BSP_LED_On(LED3);
+		else
+			BSP_LED_Off(LED3);
+		vTaskDelay(50);
 	}
 
 }
@@ -76,10 +88,26 @@ void prvDTask( void *pvParameters )
 	BSP_LED_Init(LED6);
 	for( ;; )
 	{
-		BSP_LED_Toggle(LED6);
-		vTaskDelay(168);
+		if(y < -100)
+			BSP_LED_On(LED6);
+		else
+			BSP_LED_Off(LED6);
+		vTaskDelay(50);
 	}
+}
 
+void prvAccelerometerTask( void *pvParameters )
+{
+	int16_t axes[3];
+
+	for( ;; )
+	{
+		BSP_ACCELERO_GetXYZ(axes);
+		x = axes[0];
+		y = axes[1];
+		z = axes[2];
+		vTaskDelay(50);
+	}
 }
 
 static void prvSetupHardware( void )
